0dce0b970058dc726368e900decf8731ab423bde
mariadhari6 <mariadhari6@gmail.com>
2022-07-25 22:43:14 +0700

    add: github star button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import github from "../assets/GitHub-Mark-120px-plus.png";
+import GitHubButton from "react-github-btn";
 const Footer = () => {
   return (
     <div className="absolute left-0 right-0 bottom-0 bg-gray-100">
@@ -6,12 +6,15 @@ const Footer = () => {
         <div className="text-sm">
           <p>&copy; Mar'i Adhari 2022</p>
         </div>
-        <a
-          href="https://github.com/mariadhari6"
-          className="flex items-center w-fit px-2 py-1 text-2xl"
+        <GitHubButton
+          href="https://github.com/mariadhari6/react-healthy-foods"
+          data-icon="octicon-star"
+          aria-label="Star mariadhari6/react-healthy-foods on GitHub"
+          data-show-count
+          data-size="large"
         >
-          <img src={github} alt="github" className="h-5 w-5" />
-        </a>
+          Star
+        </GitHubButton>
       </div>
     </div>
   );
