35f18342f84b1509951e8373b0fbca5bf22aa269
mariadhari6 <mariadhari6@gmail.com>
2022-07-28 15:04:48 +0700

    update: about page

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,6 +1,6 @@
 const Info = () => {
   return (
-    <div className="flex items-center absolute top-0 h-screen w-screen justify-center">
+    <div className="flex items-center absolute top-0 h-screen w-screen justify-center mx-2">
       <div className="max-w-md bg-white drop-shadow-md select-none rounded-sm p-4 flex flex-col gap-2">
         <h1 className="font-bold text-2xl">About</h1>
         <p className="text-gray-500 font-light">
