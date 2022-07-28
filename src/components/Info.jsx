7bc77f253dc59df225ab5826dd7ab3bf81b938a5
mariadhari6 <mariadhari6@gmail.com>
2022-07-28 14:43:47 +0700

    add: about page

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,8 +1,12 @@
 const Info = () => {
   return (
     <div className="flex items-center absolute top-0 h-screen w-screen justify-center">
-      <div>
-        <h1>Hello world</h1>
+      <div className="max-w-md bg-white drop-shadow-md select-none rounded-sm p-4 flex flex-col gap-2">
+        <h1 className="font-bold text-2xl">About</h1>
+        <p className="text-gray-500 font-light">
+          This is a website provided for free by the developer. This website can
+          recommend various food recipes for you to try.
+        </p>
       </div>
     </div>
   );
